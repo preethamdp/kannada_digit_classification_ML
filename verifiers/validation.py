20cff8db6708862644ec97bf806b80f24b0a1ff2
Preetham D P <35418117+preethamdp@users.noreply.github.com>
2019-11-11 00:14:48 +0530

    Update validation.py

--- verifiers/validation.py
+++ verifiers/validation.py
@@ -1,47 +1,28 @@
 import pickle
 from pandas import read_csv
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-model = pickle.load(open("./../final_model_pkl_files/knn_kannada_digits.pkl","rb"))
+model = pickle.load(open("./../final_model_pkl_files/lr_kannada_digits.pkl","rb"))
 
 dataset = read_csv("./../../../datasets/kannada_letters/train.csv")
-X_validation = dataset.values[:,1:784]
-Y_validation = dataset.values[:,0]
+X_validation = dataset.values[:,1:785].astype(float)
+Y_validation = dataset.values[:,0].astype(float)
 
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix,accuracy_score
 
-pred = model.predict(X_validation[:10000])
-Y_validation = Y_validation[:10000]
-print(accuracy_score(Y_validation,pred))
+num_folds = 10
+seed = 7
+scoring = 'accuracy'
+scaler = pickle.load(open("./../final_model_pkl_files/scaler.pkl","rb"))
+rescaledX = scaler.transform(X_validation)
+pca = pickle.load(open("./../final_model_pkl_files/pca.pkl","rb"))
+rescaledX = pca.transform(rescaledX)
+
+pred = model.predict(rescaledX)
+Y_validation = Y_validation
+print("accuracy",accuracy_score(Y_validation,pred))
 print(classification_report(Y_validation,pred))
 print(confusion_matrix(Y_validation,pred))
 
-
-#accuracy = 0.9794
-#               precision    recall  f1-score   support
-
-#            0       0.95      0.93      0.94      1000
-#            1       0.93      1.00      0.96      1000
-#            2       1.00      0.97      0.99      1000
-#            3       0.99      0.97      0.98      1000
-#            4       0.98      1.00      0.99      1000
-#            5       1.00      0.98      0.99      1000
-#            6       0.98      0.99      0.98      1000
-#            7       0.98      0.98      0.98      1000
-#            8       1.00      0.99      0.99      1000
-#            9       1.00      0.98      0.99      1000
-
-#    micro avg       0.98      0.98      0.98     10000
-#    macro avg       0.98      0.98      0.98     10000
-# weighted avg       0.98      0.98      0.98     10000
-
-# [[929  68   0   1   0   0   0   1   1   0]
-#  [  1 999   0   0   0   0   0   0   0   0]
-#  [ 17   2 975   3   1   0   1   1   0   0]
-#  [ 18   2   0 971   3   1   0   5   0   0]
-#  [  1   0   0   0 997   1   0   1   0   0]
-#  [  0   0   1   2  11 985   0   1   0   0]
-#  [  0   0   0   1   1   0 987  10   0   1]
-#  [  2   3   0   4   0   0  13 978   0   0]
-#  [  8   3   0   0   1   0   0   0 988   0]
-#  [  0   0   0   1   1   0  10   2   1 985]]
